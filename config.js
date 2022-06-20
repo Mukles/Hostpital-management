e797136e5cd773af91ef37e6811f31b850b73c60
Mukles <Mukleshossen1763@gmail.com>
2022-06-20 22:56:35 +0600

    fileUpload function done

--- config.js
+++ config.js
@@ -3,8 +3,10 @@ require("dotenv").config();
 
 const port = process.env.PORT;
 const connectionString = process.env.connectionString;
+const maxFileSize = process.env.maxFileSize;
 
 module.exports = {
   port,
   connectionString,
+  maxFileSize,
 };
